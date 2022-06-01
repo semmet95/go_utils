0c75d547b3846f6136fad318fdf0674adfd071e8
Prakash Venkiteswara <pvenkiteswara@guidewire.com>
2022-06-01 15:51:38 +0530

    first commit

---
+++ go_utils/go_utils.go
@@ -0,0 +1,50 @@
+package go_utils
+
+import (
+	"os"
+	"os/exec"
+
+	"github.com/BurntSushi/toml"
+)
+
+type BuildpackToml struct {
+	API string `toml:"api"`
+
+	Buildpack struct {
+		ID string `toml:"id"`
+		VERSION string `toml:"version"`
+	} `toml:"buildpack"`
+
+	Metadata struct {
+		TEAMNAME string `toml:"team-name"`
+		CONTACT string `toml:"contact"`
+
+		Dependencies []struct {
+			URI string `toml:"uri"`
+		} `toml:"dependencies"`
+	} `toml:"metadata"`
+
+	Stacks []struct {
+		ID string `toml:"id"`
+	} `toml:"stacks"`
+}
+
+// Execute a command and print the stdout and stderr streams
+func ExecuteCmd(cmd exec.Cmd) {
+
+}
+
+// Get full buildpack metadata
+func GetBuildpackMetadata(tomlPath string) (BuildpackToml, error) {
+	var mData BuildpackToml
+
+	file, err := os.Open(tomlPath)
+	if err != nil { return mData, err }
+
+	_, err = toml.NewDecoder(file).Decode(&mData)
+	if err != nil {
+		return mData, err
+	}
+
+	return mData, nil
+}
